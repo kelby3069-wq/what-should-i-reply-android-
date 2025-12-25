f9c05e95270f97b8ef10fd9b568de6afc88bfabc
kelby3069-wq <kelby3069@gmail.com>
2025-12-24 23:49:33 -0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,16 @@
 plugins {
-    id("com.android.application") version "8.5.2" apply false
-    id("com.android.library") version "8.5.2" apply false
+    // Android
+    id("com.android.application") version "8.7.2" apply false
 
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.24" apply false
+    // Kotlin
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
 
-    id("com.google.devtools.ksp") version "1.9.24-1.0.20" apply false
-}
+    // Kotlin 2 + Compose compiler plugin (required when compose is enabled)
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
 
-tasks.register("clean", Delete::class) {
-    delete(rootProject.layout.buildDirectory)
+    // Kotlinx Serialization plugin (must have a version somewhere)
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21" apply false
+
+    // KSP for Room compiler
+    id("com.google.devtools.ksp") version "2.0.21-1.0.26" apply false
 }
