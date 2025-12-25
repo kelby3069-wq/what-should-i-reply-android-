9ac48df51d4715ec909aa660ebc3e398a94a12f3
Kelby <kelby3069-wq@users.noreply.github.com>
2025-12-24 20:45:17 -0600

    git push origin main0Fix Android Gradle plugin resolution

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
-    // Declare plugins + versions here, but do NOT apply at root.
     id("com.android.application") version "8.5.2" apply false
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
 }
