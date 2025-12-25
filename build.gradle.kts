83e1e5eaea90e2cf29fcf5a643c7249df4cf44d1
kelby3069-wq <kelby3069@gmail.com>
2025-12-24 22:41:15 -0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,10 @@
 plugins {
-    id("com.android.application") version "8.7.0" apply false
+    id("com.android.application") version "8.7.3" apply false
+    id("com.android.library") version "8.7.3" apply false
+
+    // Match whatever Kotlin you're already using in this repo (likely 2.0+)
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
 
-    // ✅ Kotlin 2.x + Compose requires this plugin
+    // IMPORTANT: make the compose plugin available to modules
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
 }
