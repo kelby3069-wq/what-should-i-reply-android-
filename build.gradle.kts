8e2a87d904b26f7f569103ce65f904abec7f7743
kelby3069-wq <kelby3069@gmail.com>
2025-12-25 15:55:44 -0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,10 @@
 plugins {
-    id("com.android.application") version "8.13.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.10" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.10" apply false
+    // AGP compatible with Gradle 8.13
+    id("com.android.application") version "8.5.2" apply false
+
+    // Kotlin 2.x
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+
+    // ✅ REQUIRED for Compose when using Kotlin 2.0+
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
 }
