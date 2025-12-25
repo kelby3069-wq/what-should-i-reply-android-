9f3bbb282d42096e2fb8ca6715190689c543857a
kelby3069-wq <kelby3069@gmail.com>
2025-12-24 23:38:03 -0600

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,8 @@
 pluginManagement {
     repositories {
+        gradlePluginPortal()
         google()
         mavenCentral()
-        gradlePluginPortal()
     }
 }
 
@@ -14,5 +14,5 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "what-should-i-reply-android-"
+rootProject.name = "ReplySense"
 include(":app")
