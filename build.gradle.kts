00473aef1ca23319e01dae80285e7e3aec691749
kelby3069-wq <kelby3069@gmail.com>
2025-12-24 21:01:00 -0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,7 @@
 plugins {
-    id("com.android.application") version "8.5.2" apply false
+    id("com.android.application") version "8.7.0" apply false
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+
+    // ✅ Required for Kotlin 2.x + Compose
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
 }
