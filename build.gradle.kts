0b680e5093e2acb6c162dfe522edff33e0c7e03b
kelby3069-wq <kelby3069@gmail.com>
2025-12-24 19:54:19 -0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
+// Root build file (kept intentionally minimal)
 plugins {
     id("com.android.application") version "8.7.3" apply false
-    kotlin("android") version "2.0.21" apply false
-    kotlin("plugin.serialization") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21" apply false
 }
